b546d954c818859a1eb88fdf4530a0ae3c6c301c
ricardocsilveira <ricardo_silveira@live.com.pt>
2026-04-13 17:38:34 +0100

    add city logic

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 
 from database import get_db
-from models import Weather
+from models import Weather, City
 
 app = FastAPI(title="Weather API")
 
@@ -16,3 +16,27 @@ def get_weather_by_city(city_name: str, db: Session = Depends(get_db)):
 @app.get("/weather")
 def get_all_weather(db: Session = Depends(get_db)):
     return db.query(Weather).order_by(Weather.forecast_datetime.desc()).all()
+
+
+@app.get("/cities")
+def get_all_cities(db: Session = Depends(get_db)):
+    return db.query(City).all()
+
+
+@app.post("/cities")
+def add_city(name: str, db: Session = Depends(get_db)):
+    city = City(name=name)
+    db.add(city)
+    db.commit()
+    db.refresh(city)
+    return city
+
+
+@app.delete("/cities/{city_name}")
+def delete_city(city_name: str, db: Session = Depends(get_db)):
+    city = db.query(City).filter(City.name.ilike(city_name)).first()
+    if not city:
+        return {"message": "City not found"}, 404
+    db.delete(city)
+    db.commit()
+    return {"status": "deleted", "city": city_name}
--- main.py
+++
@@ -1,4 +0,0 @@
-from services import fetch_weather
-
-fetch_weather("Madrid")
-fetch_weather("Lisbon")
